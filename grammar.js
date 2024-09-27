f14d9a9a61769e0588827e23a946923a840cf51d
Peter Hill <peter.hill@york.ac.uk>
2024-09-27 14:27:08 +0100

    Update to tree-sitter 0.23

--- grammar.js
+++ grammar.js
@@ -3,5 +3,5 @@ const FORTRAN = require("tree-sitter-fortran/grammar")
 const PREC = Object.assign(FORTRAN.PREC, {})
 
 module.exports = grammar(FORTRAN, {
-    name: 'fixed_form_fortran',
+  name: 'fixed_form_fortran',
 })
